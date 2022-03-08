3d48e275ec639e683e8730263a183de1b9a9076c
Robert Hillis <tkdrob4390@yahoo.com>
2022-03-08 12:02:08 -0500

    uno mas

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,31 @@
 """Tests configuration."""
 # pylint:disable=redefined-outer-name
 import asyncio
+from typing import AsyncGenerator
+from collections.abc import Callable
 
-import pytest
+import pytest_asyncio
 from aiohttp import ClientSession
 
 from aioskybell import Skybell
 from tests import EMAIL, PASSWORD
 
 
-@pytest.fixture(autouse=True)
-def loop_factory():
+@pytest_asyncio.fixture(autouse=True)
+def loop_factory() -> Callable[..., asyncio.AbstractEventLoop]:
     """Create loop."""
     return asyncio.new_event_loop
 
 
-@pytest.fixture()
-async def apisession():
+@pytest_asyncio.fixture()
+async def apisession() -> AsyncGenerator:
     """Create client session."""
     async with ClientSession() as sess:
         yield sess
 
 
-@pytest.fixture()
-async def client(apisession):
+@pytest_asyncio.fixture()
+async def client(apisession) -> AsyncGenerator:
     """Create Client."""
     async with Skybell(
         EMAIL,
